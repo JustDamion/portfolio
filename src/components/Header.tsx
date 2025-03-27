c1c79730f219c39ea22cd091097e064b4c6c9174
JustDamion <shirkeydamion@gmail.com>
2025-03-27 00:15:40 -0500

    Added scroll-lock

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import { disablePageScroll, enablePageScroll } from "@fluejs/noscroll";
+import { useState } from "react";
 
-export default Header
+const Header = () => {
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
+
+  return <div>Header</div>;
+};
+
+export default Header;
